a24d30214d19a895f8059e531aa226b3618853d1
Matthew Taylor <rhyolight@gmail.com>
2014-10-16 09:02:59 -0700

    Ensuring caching works across multiple instances

--- sprinter.js
+++ sprinter.js
@@ -3,6 +3,7 @@ var GitHubApi = require('github')
   , async = require('async')
   , pageRegex = new RegExp("&page=(\\d*)")
   , relRegex = new RegExp("rel=\"(.*)\"")
+  , originalPrototypeFunctions = {}
   ;
 
 /**
@@ -38,11 +39,13 @@ function attachReadableErrorMessage(err) {
     }
     // 410 means repo has no GitHub Issues
     else if (err.code == 410 && err.repo) {
-        err.message = '"' + err.repo + '" has no GitHub Issues associated with it.';
+        err.message = '"' + err.repo 
+            + '" has no GitHub Issues associated with it.';
     }
     // 422 means validation error
     else if (err.code == 422 && err.repo) {
-        err.message = 'Validation error on "' + err.repo + '": ' + JSON.stringify(errorMessage.errors);
+        err.message = 'Validation error on "' + err.repo + '": '
+            + JSON.stringify(errorMessage.errors);
     }
     return err;
 }
@@ -132,8 +135,9 @@ Sprinter.prototype._cacheIsValid = function(cacheKey) {
 Sprinter.prototype._setupCaching = function() {
     var cacheDuration = this._cacheDuration
       , me = this;
-    _.each(Sprinter.prototype, function(fn, name) {
+    _.each(originalPrototypeFunctions, function(fn, name) {
         if (name.indexOf('get') == 0) {
+            console.log('wrapping %s', name);
             Sprinter.prototype[name] = function() {
 
                 // Default cache key is function name.
@@ -146,10 +150,14 @@ Sprinter.prototype._setupCaching = function() {
                     if (err) {
                         return callback(err);
                     }
-                    me._CACHE[cacheKey] = {
-                        time: new Date().getTime()
-                      , value: result
-                    };
+                    // Don't cache if duration is 0.
+                    if (me._cacheDuration) {
+                        console.log('caching response for %s', cacheKey);
+                        me._CACHE[cacheKey] = {
+                            time: new Date().getTime()
+                          , value: result
+                        };
+                    }
                     callback(err, result);
                 }
 
@@ -168,8 +176,10 @@ Sprinter.prototype._setupCaching = function() {
 
                 // If result has already been cached, use it.
                 if (me._cacheIsValid(cacheKey, cacheDuration)) {
+                    console.log('using cache for %s', cacheKey);
                     callback(null, me._CACHE[cacheKey].value);
                 } else {
+                    console.log('skipping cache for %s', cacheKey);
                     fn.apply(me, newArguments);
                 }
 
@@ -206,7 +216,8 @@ Sprinter.prototype._fetchAllPages = function(fetchFunction, params, callback) {
                 getRemainingPages(pageResults, pageCallback);
             });
         } else {
-            // Attache a repo object to each result so users can tell what repo it is coming from.
+            // Attach a repo object to each result so users can tell what repo it
+            // is coming from.
             _.each(allPages, function(item) {
                 item.repo = slug;
             });
@@ -228,6 +239,7 @@ Sprinter.prototype._fetchAllPages = function(fetchFunction, params, callback) {
  * @param duration {int} seconds to cache results.
  */
 Sprinter.prototype.setCacheDuration = function(duration) {
+    console.log('setting cache duration to %s', duration);
     this._cacheDuration = duration;
 }
 
@@ -278,7 +290,8 @@ Sprinter.prototype.getIssues = function(userFilters, mainCallback) {
         }
     };
 
-    // If the user specified only one repository to query, we don't want to query all the others.
+    // If the user specified only one repository to query, we don't want to query
+    // all the others.
     if (filters.repo) {
         filterOrg = filters.repo.split('/').shift();
         filterRepo = filters.repo.split('/').pop()
@@ -296,7 +309,9 @@ Sprinter.prototype.getIssues = function(userFilters, mainCallback) {
 Sprinter.prototype.getMilestones = function(mainCallback) {
     var me = this;
     this._eachRepoFlattened(function(org, repo, localCallback) {
-        me._fetchAllPages(me.gh.issues.getAllMilestones, {user: org, repo: repo}, localCallback);
+        me._fetchAllPages(
+            me.gh.issues.getAllMilestones, {user: org, repo: repo}, localCallback
+        );
     }, function(err, milestones) {
         if (err) {
             mainCallback(attachReadableErrorMessage(err));
@@ -395,24 +410,26 @@ Sprinter.prototype.updateMilestones = function(title, milestone, mainCallback) {
             user: org,
             repo: repo
         };
-        me._fetchAllPages(me.gh.issues.getAllMilestones, payload, function(err, milestones) {
-            var slug = org + '/' + repo;
-            if (err) {
-                localCallback(err);
-            } else {
-                var match = _.find(milestones, function(milestone) {
-                        return milestone.title == title;
-                    }),
-                    result = undefined;
-                if (match) {
-                    result = {
-                        repo: slug,
-                        number: match.number
+        me._fetchAllPages(me.gh.issues.getAllMilestones, payload, 
+            function(err, milestones) {
+                var slug = org + '/' + repo;
+                if (err) {
+                    localCallback(err);
+                } else {
+                    var match = _.find(milestones, function(milestone) {
+                            return milestone.title == title;
+                        }),
+                        result = undefined;
+                    if (match) {
+                        result = {
+                            repo: slug,
+                            number: match.number
+                        }
                     }
+                    localCallback(null, result);
                 }
-                localCallback(null, result);
             }
-        });
+        );
     }, function(err, milestonesToUpdate) {
         if (err) {
             mainCallback(attachReadableErrorMessage(err));
@@ -445,7 +462,8 @@ Sprinter.prototype.updateMilestones = function(title, milestone, mainCallback) {
 
 /**
  * Creates the same labels across all monitored repos.
- * @param labels {Array} Should be a list of objects, each with a name and hex color (without the #).
+ * @param labels {Array} Should be a list of objects, each with a name and hex 
+ *                       color (without the #).
  * @param mainCallback {function} Called with err, created labels.
  */
 Sprinter.prototype.createLabels = function(labels, mainCallback) {
@@ -485,7 +503,9 @@ Sprinter.prototype.createLabels = function(labels, mainCallback) {
 Sprinter.prototype.getLabels = function(mainCallback) {
     var me = this;
     this._eachRepoFlattened(function(org, repo, localCallback) {
-        me._fetchAllPages(me.gh.issues.getLabels, {user: org, repo: repo}, localCallback);
+        me._fetchAllPages(
+            me.gh.issues.getLabels, {user: org, repo: repo}, localCallback
+        );
     }, function(err, labels) {
         if (err) {
             mainCallback(attachReadableErrorMessage(err));
@@ -502,7 +522,9 @@ Sprinter.prototype.getLabels = function(mainCallback) {
 Sprinter.prototype.getCollaborators = function(mainCallback) {
     var me = this;
     this._eachRepoFlattened(function(org, repo, localCallback) {
-        me._fetchAllPages(me.gh.repos.getCollaborators, {user: org, repo: repo}, localCallback);
+        me._fetchAllPages(
+            me.gh.repos.getCollaborators, {user: org, repo: repo}, localCallback
+        );
     }, function(err, collaborators) {
         if (err) {
             mainCallback(attachReadableErrorMessage(err));
@@ -512,5 +534,10 @@ Sprinter.prototype.getCollaborators = function(mainCallback) {
     });
 };
 
+_.each(Sprinter.prototype, function(fn, name) {
+    if (name.indexOf('get') == 0) {
+        originalPrototypeFunctions[name] = fn;
+    }
+});
 
 module.exports = Sprinter;
